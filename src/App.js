2b652546567283adb6c383cbd4fca4d581d63c48
Adamwojty <adam12573@gmail.com>
2020-08-22 17:12:00 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+const stateMachine = {
+  initial: "initial",
+  states: {
+    initial: {
+      next: "loadingModel",
+    },
+    loadingModel: {
+      next: "awaitingUpload",
+    },
+    awaitingUpload: {
+      next: "ready",
+    },
+    ready: {
+      next: "classifying",
+      showImage: true,
+    },
+    classifying: {
+      next: "complete",
+    },
+    complete: {
+      next: "awaitingUpload",
+      showImage: true,
+      showResults: true,
+    },
+  },
+};
 
-function App() {
+const reducer = (currentState, event) =>
+  stateMachine.states[currentState][event] || stateMachine.initial;
+
+function formatResult({ className, probability }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li key={className}>{`${className}: ${(probability * 100).toFixed(2)}`}</li>
+  );
+}
+function App() {
+  tf.setBackend("cpu");
+  const [model, setModel] = React.useState(null);
+  const [imgUrl, setImgUrl] = React.useState(null);
+  const [results, setResults] = React.useState([]);
+  const [buttonProps, setButtonProps] = React.useState({
+    initial: { text: "Load Model" },
+  });
+  const [state, dispatch] = React.useReducer(reducer, stateMachine.initial);
+  const inputRef = React.useRef(null);
+  const imgRef = React.useRef(null);
+  const next = () => {
+    dispatch("next");
+  };
+  const loadModel = async () => {
+    next();
+    setButtonProps({ text: "Loading Model..." });
+    const mobilenetModel = await mobilenet.load();
+    setModel(mobilenetModel);
+    next();
+    setButtonProps({
+      text: "Upload Photo",
+      action: () => inputRef.current.click(),
+    });
+  };
+  const handleUpload = (e) => {
+    const { files } = e.target;
+    if (files.length) {
+      const url = URL.createObjectURL(files[0]);
+      setImgUrl(url);
+      setButtonProps({ text: "Identify", action: identify });
+      next();
+    }
+  };
+  const identify = async () => {
+    next();
+    setButtonProps({ text: "Identifying" });
+    const results = await model.classify(imgRef.current);
+    setResults(results);
+    next();
+    setButtonProps({
+      text: "Reset",
+      action: resetUI,
+    });
+  };
+  const resetUI = () => {
+    setResults([]);
+    setImgUrl(null);
+    next();
+    setButtonProps({
+      text: "Upload Photo",
+      action: () => inputRef.current.click(),
+    });
+  };
+
+  React.useEffect(() => {
+    loadModel();
+  }, []);
+
+  const { showImage = false, showResults = false } = stateMachine.states[state];
+  return (
+    <div className='App'>
+      {showImage ? <img src={imgUrl} alt='img-preview' ref={imgRef} /> : null}
+      {showResults ? <ul>{results.map(formatResult)}</ul> : null}
+      <input
+        type='file'
+        accept='image/*'
+        capture='camera'
+        ref={inputRef}
+        onChange={handleUpload}
+      />
+      <button onClick={buttonProps.action}>{buttonProps.text}</button>
     </div>
   );
 }
